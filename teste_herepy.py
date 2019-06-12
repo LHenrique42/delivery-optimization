35ec78833bde7e072246485b676b5b8dbe86e2aa
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-06-12 14:06:24 -0300

    testes

--- teste_herepy.py
+++ teste_herepy.py
@@ -2,6 +2,9 @@ import herepy
 
 routingApi = herepy.RoutingApi('o6i9v6u7AQdyeVTbJwWw', 'kp3lhUcL0KmT-gODan27iw')
 
-response = routingApi.truck_route([11.0, 12.0], [22.0, 23.0], [herepy.RouteMode.truck, herepy.RouteMode.fastest])
+geocoderApi = herepy.GeocoderApi('o6i9v6u7AQdyeVTbJwWw', 'kp3lhUcL0KmT-gODan27iw')
+
+#response = routingApi.truck_route([11.0, 12.0], [22.0, 23.0], [herepy.RouteMode.truck, herepy.RouteMode.fastest])
+response = geocoderApi.free_form('200 S Mathilda Sunnyvale CA')
 
 print (response)
